95f7045a00d6c6700c331671508b47d82cf54722
MadBrozzeR <git.public.s@madbr.ru>
2019-08-25 16:06:12 +0300

    Initial commit

---
+++ html/style.js
@@ -0,0 +1,127 @@
+var style = {
+  '*': {
+    boxSizing: 'border-box'
+  },
+  'html,body': {
+    height: '100%',
+    margin: 0,
+    background: '#002'
+  },
+  'input': {
+    width: '100%',
+    height: '32px',
+    lineHeight: '32px',
+    border: '1px solid #779',
+    padding: '0 10px'
+  },
+  'button': {
+    height: '32px',
+    lineHeight: '32px',
+    verticalAlign: 'top',
+    border: '1px solid #779',
+    background: '#ccf',
+    cursor: 'pointer'
+  },
+  '.full-table': {
+    width: '100%',
+    height: '100%',
+
+    '-body': {
+      verticalAlign: 'middle',
+      textAlign: 'center',
+      padding: '12px 0'
+    }
+  },
+  '.curtain': {
+    position: 'fixed',
+    top: '10%',
+    left: '70%',
+    width: '0%',
+    height: '0%',
+    opacity: 0,
+    overflow: 'hidden',
+    background: 'rgba(128, 128, 128, 0.7)',
+    transition: [
+      '.3s top ease-in-out',
+      '.3s left ease-in-out',
+      '.3s width ease-in-out',
+      '.3s height ease-in-out',
+      '.3s opacity ease-in-out'
+    ].join(', '),
+
+    '-table': {
+      width: '100%',
+      height: '100%'
+    },
+
+    '-body': {
+      verticalAlign: 'middle',
+      textAlign: 'center'
+    },
+
+    '.active': {
+      top: '0%',
+      left: '0%',
+      width: '100%',
+      height: '100%',
+      opacity: 1
+    }
+  },
+  '.auth': {
+    display: 'inline-block',
+    width: '250px',
+    background: '#ddf',
+    border: '1px solid #779',
+    padding: '24px',
+    overflow: 'hidden',
+
+    '-submit': {
+      width: '100%'
+    }
+  },
+
+  '.form': {
+    display: 'inline-block',
+    maxWidth: '1000px',
+    width: '100%',
+    background: '#ddf',
+    padding: 24 * 2 + 30 + 35 + 'px 24px 24px',
+    height: '100%'
+  },
+  '.command': {
+    '-row': {
+      paddingRight: '150px',
+      marginTop: -24 - 30 - 35 + 'px',
+      marginBottom: '24px'
+    },
+    '-input': {
+    },
+    '-submit': {
+      width: '150px',
+      marginRight: '-150px'
+    }
+  },
+  '.output': {
+    height: '100%',
+    background: '#eef',
+    border: '1px solid #779',
+    whiteSpace: 'pre-wrap',
+    textAlign: 'left',
+    padding: '12px',
+    overflow: 'auto'
+  },
+  '.toolbar': {
+    textAlign: 'left',
+
+    '-button': {
+      display: 'inline-block',
+      lineHeight: '30px',
+      border: '1px solid #779',
+      background: '#ccf',
+      width: '100px',
+      margin: '0 3px 3px 0',
+      padding: '0 5px',
+      cursor: 'pointer'
+    }
+  }
+};
---
+++ server.js
@@ -0,0 +1,104 @@
+const MadSocket = require('madsocket');
+const Rcon = require('mbr-rcon');
+
+const clients = {
+  all: {},
+  index: 0,
+  add: function (client) {
+    client.params = {id: this.index++};
+    this.all[client.params.id] = client;
+  },
+  del: function (client) {
+    delete this.all[client.params.id];
+  }
+};
+
+const rcon = new Rcon({
+  host: 'localhost',
+  port: 25001
+});
+
+function send (type, data) {
+  this.send(JSON.stringify({type, data}));
+}
+
+const WS = new MadSocket({
+  connect: function () {
+    const client = this;
+    clients.add(this);
+    client.params.connection = rcon.connect({
+      onError: function (error) {
+        console.log(client.params, error);
+        send.call(client, 'error', 'Failed to connect to remote console');
+      },
+      onSuccess: function () {
+        send.call(client, 'auth', 'ready');
+      }
+    });
+  },
+  disconnect: function () {
+    client.params.connection.close();
+    clients.del(this);
+  },
+  message: function (message) {
+    let data;
+    const client = this;
+    try {
+      data = JSON.parse(message);
+    } catch (error) {
+      data = {};
+    }
+
+    switch (data.type) {
+      case 'auth':
+        client.params.connection.auth({
+          password: data.data,
+
+          onSuccess: function () {
+            send.call(client, 'auth', 'success');
+          },
+          onError: function () {
+            send.call(client, 'error', 'Authentication failed');
+          }
+        });
+        break;
+      case 'command':
+        client.params.connection.send(data.data, {
+          onSuccess: function (data) {
+            send.call(client, 'response', data);
+          }
+        });
+        break;
+      default:
+        send.call(client, 'error', 'Wrong data format');
+    }
+  }
+});
+
+const page404Template = {
+  title: '404 - Page Not Found',
+  body: '<body>404 - Page Not Found</body>'
+}
+
+function get404Page () {
+  this.status = 404;
+  this.send(this.template(page404Template), 'html');
+}
+
+function getSocketConnection () {
+  WS.leach(this.request, this.response);
+}
+
+const routes = {
+  '/': __dirname + '/html/index.html',
+  '/src/mbr-script.js': __dirname + '/node_modules/mbr-script/index.js',
+  '/favicon.ico': __dirname + '/html/m-favicon.ico',
+  '/src/style.js': __dirname + '/html/style.js',
+  '/ws': getSocketConnection,
+
+  default: get404Page
+};
+
+module.exports = function (request) {
+  request.route(routes);
+}
